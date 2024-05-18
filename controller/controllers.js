2335879fcd8221bf5ef3e67589860c1953179890
godcandidate <godcandidate@gmail.com>
2024-05-18 12:06:05 +0000

    update controller.js feat: Use addDoc in createUser function for auto-generated user IDs in Firestore

--- controller/controllers.js
+++ controller/controllers.js
@@ -1,27 +1,24 @@
 //controller.js
-const { collection, getFirestore, doc, setDoc } = require("firebase/firestore"); // (Optional, if not already imported)
+const { collection, addDoc } = require("firebase/firestore"); // (Optional, if not already imported)
 const db = require("../firestore"); // Import the Firestore instance
 
-// Create the collections
-//const UserCollection = collection(db, "Users");
-
-
+// Create user details
 async function  createUser(req, res){
     try {
         const userData = req.body;
         
         if (userData) {
-            const userRef = doc(db, "Users", "C"); 
-            await setDoc(userRef, userData , { merge: true });
+            await addDoc(collection(db, "Users"), userData);
             return res.status(200).send({ msg: "User Info added successfully", userData  });
         } else {
-            return res.status(400).send({ error: "Missing data" });
+            return res.status(400).send({ error: "Missing data, user data was not added" });
         }
     } catch (error) {
-        console.error("Error adding user:", error);
-        // ... (rest of your catch block)
+        return res.status(500).send({ error: "User not created" });
     }
 };
+
+
 
 module.exports = { createUser };
 

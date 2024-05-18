ca583671c4196b0c5fb21fa46946ee2c65c1d28c
godcandidate <godcandidate@gmail.com>
2024-05-18 11:49:55 +0000

    feat: Add createUser function for Firestore

--- controller/controllers.js
+++ controller/controllers.js
@@ -1,14 +1,25 @@
+//controller.js
+const { collection, getFirestore, doc, setDoc } = require("firebase/firestore"); // (Optional, if not already imported)
+const db = require("../firestore"); // Import the Firestore instance
+
+// Create the collections
+//const UserCollection = collection(db, "Users");
+
+
 async function  createUser(req, res){
     try {
-        const data = req.body;
+        const userData = req.body;
         
-        if (data) {
-            return res.status(200).send({ msg: "User Info added successfully" });
+        if (userData) {
+            const userRef = doc(db, "Users", "C"); 
+            await setDoc(userRef, userData , { merge: true });
+            return res.status(200).send({ msg: "User Info added successfully", userData  });
         } else {
-            return res.status(500).send({ error: "User was not added" });
+            return res.status(400).send({ error: "Missing data" });
         }
     } catch (error) {
-        return res.status(500).send({ error: "Internal server error" })
+        console.error("Error adding user:", error);
+        // ... (rest of your catch block)
     }
 };
 
